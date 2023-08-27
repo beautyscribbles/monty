f1d6dafec3b0dd03a7e2281fbd7e86f99ed5d92c
flh6 <talk2florencehanson@gmail.com>
2023-08-27 14:00:41 -0700

    pop, swap, sub functionality

--- opcode_func.2.c
+++ opcode_func.2.c
@@ -49,7 +49,7 @@ void _add(stack_t **doubly, unsigned int cline)
 
 	if (m < 2)
 	{
-		dprintf(2, "L%u: can't add, stack too short\n", cline);
+		vprintf(2, "L%u: can't add, stack too short\n", cline);
 		free_vglo();
 		exit(EXIT_FAILURE);
 	}
